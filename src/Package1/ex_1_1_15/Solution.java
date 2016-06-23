81a937cbec62b2edbb780abfe2452256d221a447
vovan12345 <vovan-number1@mail.ru>
2016-06-23 21:10:57 +0600

    New Solution 23.06.2016 added stringout

--- src/Package1/ex_1_1_15/Solution.java
+++ src/Package1/ex_1_1_15/Solution.java
@@ -4,4 +4,9 @@ package Package1.ex_1_1_15;
  * Created by vl on 23.06.16.
  */
 public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Exercise 1");
+
+    }
 }
