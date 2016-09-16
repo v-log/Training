86848152032145a35e8b6fb9762acf3553a0e5f9
v-log <v-log1313@mail.ru>
2016-09-16 21:27:08 +0700

    Переменная gcd в конструкторе переименована в gcdTemp. Аргументы конструктора названы отлично от полей класса. Явное умножение на (-1) заменено на унарный минус. Убрано дублирование проверок на НОД числителя и знаменателя. Вычитание и деление реализовано через сложение и умножение соответственно. Убраны assert'ы за ненадобностью.

--- src/Ex_1_2_16_1_2_17/Rational.java
+++ src/Ex_1_2_16_1_2_17/Rational.java
@@ -67,23 +67,21 @@ public class Rational {
     private final long numer;
     private final long denom;
 
-    Rational(long numer, long denom) throws ArithmeticException {
-        long gcd = gcd( numer, denom );
+    Rational(long num, long den) throws ArithmeticException {
+        long gcdTemp = gcd( num, den );
 
-        if (denom == 0) throw new ArithmeticException();
+        if (den == 0) throw new ArithmeticException();
 
-        if ( gcd > 1 ) {
-            numer = numer / gcd;
-            denom = denom / gcd;
+        if ( gcdTemp > 1 ) {
+            num = num / gcdTemp;
+            den = den / gcdTemp;
         }
-
-        if (denom < 0) {
-            this.numer = numer * (-1);
-            this.denom = denom * (-1);
-        }
-        else {
-            this.numer = numer;
-            this.denom = denom;
+        if (den < 0) {
+            numer = -num;
+            denom = -den;
+        } else {
+            numer = num;
+            denom = den;
         }
     }
 
@@ -93,12 +91,10 @@ public class Rational {
     public long denominator()
     { return this.denom; }
 
-    // Сумма this и that
+    // Сумма this и b
     public Rational plus(Rational b) {
         long sumNumer;
         long sumDenom;
-        long tempNumer;
-        long tempDenom;
         Rational sum;
         long aN = this.numer;
         long aD = this.denom;
@@ -113,123 +109,43 @@ public class Rational {
             // Если знаменатели различны, перемножаем крест-накрест числители и знаменатели
             long t1 = sTimes(aN, bD);
             long t2 = sTimes(bN, aD);
-            // Временный числитель (так как потом у числителя
-            // и знаменателя может быть общий множитель)
-            tempNumer = sPlus(t1, t2);
+            sumNumer = sPlus(t1, t2);
             long gcdDenom = gcd(aD, bD);
 
             if ( gcdDenom > 1) {
-                // Если у знаменателей есть общий множитель,
-                // находим временный общий знаменатель:
+                // Если у знаменателей есть НОД,
+                // находим общий знаменатель:
                 // перемножаем знаменатели и
-                // делим произведение на общий множитель
-                tempDenom = sTimes(aD, bD) / gcdDenom;
+                // делим произведение на НОД
+                sumDenom = sTimes(aD, bD) / gcdDenom;
                 // Находим числители чисел
-                long t3 = sTimes(aN, tempDenom) / aD;
-                long t4 = sTimes(bN, tempDenom) / bD;
-                // Временный числитель
-                tempNumer = sPlus(t3, t4);
+                long t3 = sTimes(aN, sumDenom) / aD;
+                long t4 = sTimes(bN, sumDenom) / bD;
+                // Числитель
+                sumNumer = sPlus(t3, t4);
             }
-            // Если у знаменателей нет общего множителя,
-            // перемножаем их
-            else tempDenom = sTimes(aD, bD);
+            else sumDenom = sTimes(aD, bD);
 
-            long gcdSum = gcd(tempNumer, tempDenom);
-
-            if ( gcdSum > 1) {
-                // Если у числителя и знаменателя есть общий
-                // множитель, сокращаем их
-                sumNumer = tempNumer / gcdSum;
-                sumDenom = tempDenom / gcdSum;
-            }
-            else {
-                sumNumer = tempNumer;
-                sumDenom = tempDenom;
-            }
             sum = new Rational(sumNumer, sumDenom);
         }
         return sum;
     }
 
-    // Вычитание из this that
+    // Вычитание из this b
     public Rational minus(Rational b) {
-        long subNumer;
-        long subDenom;
-        long tempNumer;
-        long tempDenom;
-        Rational sub;
-        long aN = this.numer;
-        long aD = this.denom;
-        long bN = b.numer;
-        long bD = b.denom;
-
-        if (aD == bD) {
-            subNumer = sMinus(aN, bN);
-            sub = new Rational(subNumer, aD);
-        }
-        else{
-            // Если знаменатели различны, перемножаем крест-накрест числители и знаменатели
-            long t1 = sTimes(aN, bD);
-            long t2 = sTimes(bN, aD);
-            // Временный числитель (так как потом у числителя
-            // и знаменателя может быть общий множитель)
-            tempNumer = sMinus(t1, t2);
-            long gcdDenom = gcd(aD, bD);
-
-            if ( gcdDenom > 1) {
-                // Если у знаменателей есть общий множитель,
-                // находим временный общий знаменатель:
-                // перемножаем знаменатели и
-                // делим произведение на общий множитель
-                tempDenom = sTimes(aD, bD) / gcdDenom;
-                // Находим числители чисел
-                long t3 = sTimes(aN, tempDenom) / aD;
-                long t4 = sTimes(bN, tempDenom) / bD;
-                // Временный числитель
-                tempNumer = sMinus(t3, t4);
-            }
-            // Если у знаменателей нет общего множителя,
-            // перемножаем их
-            else tempDenom = sTimes(aD, bD);
-
-            long gcdSub = gcd(tempNumer, tempDenom);
-
-            if ( gcdSub > 1) {
-                // Если у числителя и знаменателя есть общий
-                // множитель, сокращаем их
-                subNumer = tempNumer / gcdSub;
-                subDenom = tempDenom / gcdSub;
-            }
-            else {
-                subNumer = tempNumer;
-                subDenom = tempDenom;
-            }
-            sub = new Rational(subNumer, subDenom);
-        }
-        return sub;
+        Rational bTemp = new Rational(-b.numer, b.denom);
+        return this.plus(bTemp);
     }
 
-    // Умножение this на that
+    // Умножение this на b
     public Rational times(Rational b) {
         long timesNumer;
         long timesDenom;
-        long tempNumer;
-        long tempDenom;
         Rational times;
 
-        tempNumer = sTimes(this.numer, b.numer);
-        tempDenom = sTimes(this.denom, b.denom);
+        timesNumer = sTimes(this.numer, b.numer);
+        timesDenom = sTimes(this.denom, b.denom);
 
-        long gcdTimes = gcd(tempNumer, tempDenom);
-
-        if ( gcdTimes > 1 ) {
-            timesNumer = tempNumer / gcdTimes;
-            timesDenom = tempDenom / gcdTimes;
-        }
-        else {
-            timesNumer = tempNumer;
-            timesDenom = tempDenom;
-        }
         times = new Rational(timesNumer, timesDenom);
 
         return times;
@@ -237,88 +153,29 @@ public class Rational {
 
     // Деление this на that
     public Rational divides(Rational b) {
-        long divNumer;
-        long divDenom;
-        long tempNumer;
-        long tempDenom;
-        Rational div;
-
-        tempNumer = sTimes(this.numer, b.denom);
-        tempDenom = sTimes(this.denom, b.numer);
-
-        long gcdDiv = gcd(tempNumer, tempDenom);
-
-        if ( gcdDiv > 1 ) {
-            divNumer = tempNumer / gcdDiv;
-            divDenom = tempDenom / gcdDiv;
-        }
-        else {
-            divNumer = tempNumer;
-            divDenom = tempDenom;
-        }
-        div = new Rational(divNumer, divDenom);
-
-        return div;
+        Rational bTemp = new Rational(b.denom, b.numer);
+        return this.times(bTemp);
     }
 
     // Проверка на равенство this и that
     public boolean equals(Object x) {
-        long gcdThis;
-        long thisNumer;
-        long thisDenom;
-        long gcdThat;
-        long thatNumer;
-        long thatDenom;
 
         if (this == x) return true;
         if (x == null) return false;
         if (this.getClass() != x.getClass()) return false;
 
         Rational that = (Rational) x;
-        long aN = this.numer;
-        long aD = this.denom;
-        long bN = that.numer;
-        long bD = that.denom;
 
-        gcdThis = gcd(aN, aD);
-
-        if ( gcdThis > 1 ) {
-            thisNumer = aN / gcdThis;
-            thisDenom = aD / gcdThis;
-        }
-        else {
-            thisNumer = aN;
-            thisDenom = aD;
-        }
-
-        gcdThat = gcd(bN, bD);
-
-        if ( gcdThat > 1 ) {
-            thatNumer = bN / gcdThat;
-            thatDenom = bD / gcdThat;
-        }
-        else {
-            thatNumer = bN;
-            thatDenom = bD;
-        }
-
-        return thisNumer == thatNumer && thisDenom == thatDenom;
+        return this.numer == that.numer && this.denom == that.denom;
     }
 
     // Safe plus
     public static long sPlus(final long a, final long b) throws ArithmeticException {
         long c = a + b;
-        assert ((a & b & ~c) | (~a & ~b & c)) >= 0 : "long overflow sPlus(" + a + ", " + b + ")";
         if (((a & b & ~c) | (~a & ~b & c)) < 0) {
             throw new ArithmeticException("long overflow sPlus(" + a + ", " + b + ")");
         }
         else return c;
-    }
-
-    // Safe minus
-    public static  long sMinus(final long a, final long b) {
-        if (( a < 0 && b > 0 ) || ( a > 0 && b < 0 )) return sPlus(a, -b);
-        else return a - b;
     }
 
     // Safe times
@@ -326,7 +183,6 @@ public class Rational {
         if ( a == 0 || b == 0 ) return 0;
         long c = a * b;
         long c2 = c / b;
-        assert (a == c2) : "long overflow sTimes(" + a + ", " + b + ")";
         if (a != c2) {
             throw new ArithmeticException("long overflow sTimes(" + a + ", " + b + ")");
         }
