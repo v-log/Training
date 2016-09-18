38ed90ac3c80ed0d684ab991c76c3271c073dccc
v-log <v-log1313@mail.ru>
2016-09-18 09:28:23 +0700

    Добавлены тесты для parenthesesBalancedTest() c пустой строкой, ><, }{.

--- tests/Ex_1_3_4/ParenthesesTest.java
+++ tests/Ex_1_3_4/ParenthesesTest.java
@@ -12,10 +12,28 @@ import static org.junit.Assert.assertTrue;
 public class ParenthesesTest {
     @Test
     public void parenthesesBalancedTest() throws Exception {
+        // Тест на успешную проверку баланса скобок
         String input1 = "{([])}";
         boolean res1 = parenthesesBalanced(input1);
         assertTrue(res1);
 
+        String input8 = "[()]{}{[()()]()}";
+        boolean res8 = parenthesesBalanced(input8);
+        assertTrue(res8);
+
+        String input4 = "asd";
+        boolean res4 = parenthesesBalanced(input4);
+        assertTrue(res4);
+
+        String input6 = "";
+        boolean res6 = parenthesesBalanced(input6);
+        assertTrue(res6);
+
+        String input7 = "><";
+        boolean res7 = parenthesesBalanced(input7);
+        assertTrue(res7);
+
+        // Тест на НЕуспешную проверку баланса скобок
         String input2 = "([)]";
         boolean res2 = parenthesesBalanced(input2);
         assertFalse(res2);
@@ -24,13 +42,16 @@ public class ParenthesesTest {
         boolean res3 = parenthesesBalanced(input3);
         assertFalse(res3);
 
-        String input4 = "asd";
-        boolean res4 = parenthesesBalanced(input4);
-        assertTrue(res4);
-
         String input5 = "asd}";
         boolean res5 = parenthesesBalanced(input5);
         assertFalse(res5);
-    }
 
+        String input9 = "{asd";
+        boolean res9 = parenthesesBalanced(input9);
+        assertFalse(res9);
+
+        String input10 = "}{";
+        boolean res10 = parenthesesBalanced(input10);
+        assertFalse(res10);
+    }
 }
