9e43d4d01d376a5b37ce2745dae4dd4d0ce190bc
v-log <v-log1313@mail.ru>
2016-09-24 05:48:46 +0700

    Решение задачи 1.3.10

--- src/Ex_1_3_10/InfixToPostfix.java
+++ src/Ex_1_3_10/InfixToPostfix.java
@@ -11,177 +11,191 @@ infix to postfix. */
 public class InfixToPostfix {
 
     public static void main(String[] args) {
-        String inStr1 = "A * B + C * D";
-        String inStr2 = "A + B * C + D";
-        String inStr3 = "A + B + C + D";
-        String inStr4 = "(A + B) * (C + D)";
-        String inStr5 = "(A + B + K) * (C + D)";
-        String inStr6 = "(A ^ B + K) * (C + D)";
-        String inStr7 = "(A + B * K) * (C + D)";
-        String inStr8 = "(A * B + C) + D * E";
-        String inStr9 = "(A + B * C) + D * E";
-        String inStr10 = "A * B ^ C";
-        String inStr11 = "(A + B * C + D) * (E + K * F * ( H + I ) )";
-        String inStr12 = "A + B * C + D * (E + F ^ G * H + I * J ^ K * L * (M ^ N * (P ^ Q + R)))";
-        String outStr1 = convertInfixToPostfix(inStr1);
-        String outStr2 = convertInfixToPostfix(inStr2);
-        String outStr3 = convertInfixToPostfix(inStr3);
-        String outStr4 = convertInfixToPostfix(inStr4);
-        String outStr5 = convertInfixToPostfix(inStr5);
-        String outStr6 = convertInfixToPostfix(inStr6);
-        String outStr7 = convertInfixToPostfix(inStr7);
-        String outStr8 = convertInfixToPostfix(inStr8);
-        String outStr9 = convertInfixToPostfix(inStr9);
-        String outStr10 = convertInfixToPostfix(inStr10);
-        String outStr11 = convertInfixToPostfix(inStr11);
-        String outStr12 = convertInfixToPostfix(inStr12);
-        System.out.println(inStr1 + "  =  " + outStr1);
-        System.out.println(inStr2 + "  =  " + outStr2);
-        System.out.println(inStr3 + "  =  " + outStr3);
-        System.out.println(inStr4 + "  =  " + outStr4);
-        System.out.println(inStr5 + "  =  " + outStr5);
-        System.out.println(inStr6 + "  =  " + outStr6);
-        System.out.println(inStr7 + "  =  " + outStr7);
-        System.out.println(inStr8 + "  =  " + outStr8);
-        System.out.println(inStr9 + "  =  " + outStr9);
-        System.out.println(inStr10 + "  =  " + outStr10);
-        System.out.println(inStr11 + "  =  " + outStr11);
-        System.out.println(inStr12 + "  =  " + outStr12);
+        try {
+            String inStr1 = "A * B + C * D";
+            String inStr2 = "A + B * C + D";
+            String inStr3 = "A + B + C + D";
+            String inStr4 = "(A + B) * (C + D)";
+            String inStr5 = "(A + B + K) * (C + D)";
+            String inStr6 = "(A ^ B + K) * (C + D)";
+            String inStr7 = "(A + B * K) * (C + D)";
+            String inStr8 = "(A * B + C) + D * E";
+            String inStr9 = "(A + B * C) + D * E";
+            String inStr10 = "A * B ^ C";
+            String inStr11 = "(A + B * C + D) * (E + K * F * ( H + I ) )";
+            String inStr12 = "A + B * C + D * (E + F ^ G * H + I * J ^ K * L * (M ^ N * (P ^ Q + R)))";
+            String inStr13 = "(A)";
+
+            System.out.println(inStr1 + "  =  " + convINFIXtoPostfix(inStr1));
+            System.out.println(inStr2 + "  =  " + convINFIXtoPostfix(inStr2));
+            System.out.println(inStr3 + "  =  " + convINFIXtoPostfix(inStr3));
+            System.out.println(inStr4 + "  =  " + convINFIXtoPostfix(inStr4));
+            System.out.println(inStr5 + "  =  " + convINFIXtoPostfix(inStr5));
+            System.out.println(inStr6 + "  =  " + convINFIXtoPostfix(inStr6));
+            System.out.println(inStr7 + "  =  " + convINFIXtoPostfix(inStr7));
+            System.out.println(inStr8 + "  =  " + convINFIXtoPostfix(inStr8));
+            System.out.println(inStr9 + "  =  " + convINFIXtoPostfix(inStr9));
+            System.out.println(inStr10 + "  =  " + convINFIXtoPostfix(inStr10));
+            System.out.println(inStr11 + "  =  " + convINFIXtoPostfix(inStr11));
+            System.out.println(inStr12 + "  =  " + convINFIXtoPostfix(inStr12));
+            System.out.println(inStr13 + "  =  " + convINFIXtoPostfix(inStr13));
+
+            convINFIXtoPostfix("   ");
+        }
+        catch (IllegalArgumentException e1) {
+            System.out.println(e1.getMessage());
+        }
     }
 
-    private static final String opsPlus = "+-";
-    private static final String opsMultip = "*/";
-    private static final char OP_POW = '^';
-    private static final char L_PAR = '(';
-    private static final char R_PAR = ')';
-    private static String output;
+    private static final String opsStr = "()+-*/^";
 
-    public static String convertInfixToPostfix(String infix) {
+    static class Operator {
+        private final char opSymb;
+        private final int prec;
 
-        ResizingArrayStack<Character> ops = new ResizingArrayStack<>();
+        Operator(Character opSymbol) throws IllegalArgumentException {
+
+        // Проверка допустимости введенного оператора
+            if (opsStr.indexOf(opSymbol) < 0) {
+                throw new IllegalArgumentException("Illegal operation \"" + opSymbol + "\"");
+            }
+
+        // Назначение приоритета оператору из введенной строки:
+        // () = 0;  +- = 1;  */ = 2;  ^ = 3;
+            int precedence = opsStr.indexOf(opSymbol) / 2;
+            opSymb = opSymbol;
+            prec = precedence;
+        }
+
+        char opSymb()
+        { return this.opSymb; }
+
+        int prec()
+        { return this.prec; }
+    }
+
+// Результирующая строка для конвертера
+    private static StringBuilder output = new StringBuilder();
+
+
+    static String convINFIXtoPostfix(String infix) throws IllegalArgumentException,
+            NullPointerException {
+
+        if(infix == null) throw new NullPointerException("Expression can't be null");
+
+        if(infix.isEmpty()) throw new IllegalArgumentException("No expression to process");
+
+    // Очищаем рез.строку
+        output.setLength(0);
+
+    // Стэк для операторов
+        ResizingArrayStack<Operator> ops = new ResizingArrayStack<>();
+
         char[] infixChars = infix.toCharArray();
-        output = "";
-        int hiIsOnStack = 0;
-        int parFlag = 0;
-        int powFlag = 0;
-        for (char ch: infixChars) {
 
-            if (Character.toString(ch).matches("\\p{L}")) {
-                output = output + ch + " ";
+
+    // Флаг наличия операнда перед оператором
+        boolean operandIsLast = false;
+    // Флаг наличия оператора перед операндом
+        boolean operatorIsLast = false;
+    // Счетчик открытых скобок
+        int parOpenCount = 0;
+    // Флаг наличия операнда после открытой скобки
+        boolean operandAfterPar = false;
+
+
+        for (char chArg : infixChars) {
+
+        // Если символ из введенной строки - операнд, добавляем его к выходной строке
+            if (Character.toString(chArg).matches("\\p{L}")) {
+
+            // Проверка на наличие оператора перед текущим операндом
+                if ( !operatorIsLast && output.length() != 0 ) {
+                    throw new IllegalArgumentException("Missing operation before \"" + chArg + "\"");
+                }
+            // Фиксирование наличия операнда после открытой скобки необходимо для проверки
+            // правильности введенного выражения
+                if ( !ops.isEmpty() && opsStr.indexOf(ops.peek().opSymb()) == 0 ) operandAfterPar = true;
+
+                opOut(output, chArg);
+                operandIsLast = true;
+                operatorIsLast = false;
                 continue;
             }
 
-            if(ch == L_PAR) {
-                hiIsOnStack = 0;
+        // Если символ - пробел, идем дальше по строке
+            if (chArg == ' ') continue;
+
+        // Создаем объект с оператором из введенной строки с соответствующим приоритетом
+            Operator ch = new Operator(chArg);
+
+
+        // Если символ - левая скобка, добавляем его в стек и идем дальше по строке
+            if ( opsStr.indexOf(ch.opSymb()) == 0) {
                 ops.push(ch);
-                parFlag = 1;
+                parOpenCount++;
                 continue;
             }
 
-            if(ch == R_PAR) {
-                while(!ops.isEmpty()) {
 
-                    char opOut = ops.pop();
-                    if (opOut == L_PAR) break;
-                    output = output + opOut + " ";
+        // Если символ - правая скобка, выводим из стэка в рез.строку операторы,
+        // пока не дойдем до первой левой скобки
+            if ( opsStr.indexOf(ch.opSymb()) == 1 ) {
 
+            // Проверка на наличие открывающей скобки
+                if (parOpenCount < 1) throw new IllegalArgumentException("Missing opening parenthesis");
+
+                while (!ops.isEmpty() && opsStr.indexOf(ops.peek().opSymb()) != 0) {
+                    opOut(output, ops.pop().opSymb());
                 }
-
-                hiIsOnStack = 0;
-                parFlag = 0;
+                ops.pop();
+                parOpenCount--;
                 continue;
             }
 
-            if (opsMultip.indexOf(ch) >= 0) {
 
-//                if (ops.isEmpty()) hiIsOnStack = 1;
-//                ops.push(ch);
-                if (powFlag == 1) {
-//                    while(!ops.isEmpty()) {
-//                        if (ops.peek() == L_PAR) break;
-//                        char opOut = ops.pop();
-//                        output = output + opOut + " ";
-//                    }
-                    char opOut = ops.pop();
-                    output = output + opOut + " ";
-                    powFlag = 0;
-//                    ops.push(ch);
-//                    continue;
+        // Если символ - оператор:
+            if ( opsStr.indexOf(ch.opSymb()) > 1 ) {
+
+            // Проверка на наличие операнда перед текущим оператором
+                if (!operandIsLast) {
+                    throw new IllegalArgumentException("Missing operand before \"" + chArg + "\"");
                 }
-//                if(parFlag == 1) ops.push(ch);
-                if (hiIsOnStack == 1) {
-                    output = output + ch + " ";
-                    hiIsOnStack = 1;
-                    parFlag = 0;
-                    continue;
-                    /*while(!ops.isEmpty()) {
-                    if (ops.peek() == L_PAR) break;
-                    char opOut = ops.pop();
-                    output = output + opOut + " ";*/
+            // Проверка на наличие после открытой скобки операнда перед оператором
+                if (parOpenCount > 0 && !ops.isEmpty() && opsStr.indexOf(ops.peek().opSymb()) == 0
+                        && !operandAfterPar) {
+                    throw new IllegalArgumentException("Missing operand before \"" + chArg + "\"");
                 }
 
-                ops.push(ch);
-                hiIsOnStack = 1;
-                parFlag = 0;
-                powFlag = 0;
-                continue;
-            }
+                operandAfterPar = false;
+                operatorIsLast = true;
+                operandIsLast = false;
 
-            if (opsPlus.indexOf(ch) >= 0) {
-//                if ( (parFlag !=1 && hiIsOnStack != 1 || !ops.isEmpty()))
-//                    while(!ops.isEmpty()) {
-//                        char opOut = ops.pop();
-//                        output = output + opOut + " ";
-//                    }
-//                ops.push(ch);
-//                hiIsOnStack = 0;
-//                if (powFlag == 1) {
-//                    while (!ops.isEmpty()) {
-//                        if (ops.peek() == L_PAR) break;
-//                        char opOut = ops.pop();
-//                        output = output + opOut + " ";
-//                    }
-//                    powFlag = 0;
-////                    ops.push(ch);
-////                    continue;
-//                }
-
-                if (parFlag == 1) {
-                    ops.push(ch);
-                    parFlag = 0;
-                    continue;
-                }
-
-                if (  !ops.isEmpty() ) while(!ops.isEmpty()) {
-                    if (ops.peek() == L_PAR) break;
-                    char opOut = ops.pop();
-                    output = output + opOut + " ";
+            // Выводим из стэка в рез.строку операторы, пока оператор из введенной строки
+            // не будет иметь больший приоритет, чем оператор в стэке;
+                while (!ops.isEmpty() && ops.peek().prec() >= ch.prec()) {
+                    opOut(output, ops.pop().opSymb());
                 }
                 ops.push(ch);
-
-//                parFlag = 0;
-
-                hiIsOnStack = 0;
-                parFlag = 0;
-                powFlag = 0;
-                continue;
             }
-
-            if (ch == OP_POW) {
-                ops.push(ch);
-                parFlag = 0;
-                powFlag = 1;
-                continue;
-            }
-
-            if (ch == ' ') continue;
-
-            throw new IllegalArgumentException("Illegal argument: " + ch);
         }
-        while(!ops.isEmpty()) {
-            char opOut = ops.pop();
-            output = output + opOut + " ";
+
+    // Проверка на то, что последним в выражении, не считая скобок, был операнд, а не оператор
+        if (!operandIsLast) throw new IllegalArgumentException("Missing last operand");
+    // Проверка баланса скобок
+        if (parOpenCount != 0) throw new IllegalArgumentException("Missing closing parentheses");
+
+
+    // Выводим в рез.строку оставшиеся в стэке операторы
+        while (!ops.isEmpty()) {
+            opOut(output, ops.pop().opSymb());
         }
-        return output;
+
+        return output.toString();
+    }
+
+    static void opOut(StringBuilder outp, char oper) {
+        outp.append(oper);
+        outp.append(" ");
     }
 }
--- tests/Ex_1_3_10/InfixToPostfixTest.java
+++ tests/Ex_1_3_10/InfixToPostfixTest.java
@@ -2,100 +2,264 @@ package Ex_1_3_10;
 
 import org.junit.Test;
 
-import static Ex_1_3_10.InfixToPostfix.convertInfixToPostfix;
+import static Ex_1_3_10.InfixToPostfix.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * Created by vl on 20.09.16.
  */
 public class InfixToPostfixTest {
     @Test
-    public void convertInfixToPostfixTest() throws Exception {
-        String inStr1 = "A * B + C * D";
-        String inStr2 = "A + B * C + D";
-        String inStr3 = "A + B + C + D";
-        String inStr4 = "(A + B) * (C + D)";
-        String inStr5 = "(A + B + K) * (C + D)";
-        String inStr6 = "(A ^ B + K) * (C + D)";
-        String inStr7 = "(A + B * K) * (C + D)";
-        String inStr8 = "(A * B + C) + D * E";
-        String inStr9 = "(A + B * C) + D * E";
-        String inStr10 = "A * B ^ C";
-        String inStr11 = "(A + B * C + D) * (E + K * F * ( H + I ) )";
-        String inStr12 = "A + B * C + D * (E + F ^ G * H + I * J ^ K * L * (M ^ N * (P ^ Q + R)))";
+    public void convertINFIXToPostfixTest() throws Exception {
 
-        String inStr20 = "A + B * C";
-        String outStr20 = convertInfixToPostfix(inStr20);
-        assertEquals("A B C * + ", outStr20);
+    // Тест на успешную конвертацию
+        assertEquals("A B C * + ", convINFIXtoPostfix("A + B * C"));
+        assertEquals("A B * C - ", convINFIXtoPostfix("A * B - C"));
 
-        String inStr21 = "A * B + C";
-        String outStr21 = convertInfixToPostfix(inStr21);
-        assertEquals("A B * C + ", outStr21);
+        assertEquals("A B C ^ + ", convINFIXtoPostfix("A + B ^ C"));
+        assertEquals("A B ^ C - ", convINFIXtoPostfix("A ^ B - C"));
 
-        String inStr22 = "A + B ^ C";
-        String outStr22 = convertInfixToPostfix(inStr22);
-        assertEquals("A B C ^ + ", outStr22);
+        assertEquals("A B C ^ * ", convINFIXtoPostfix("A * B ^ C"));
+        assertEquals("A B ^ C / ", convINFIXtoPostfix("A ^ B / C"));
 
-        String inStr23 = "A ^ B + C";
-        String outStr23 = convertInfixToPostfix(inStr23);
-        assertEquals("A B ^ C + ", outStr23);
+        assertEquals("A B + C + D * ", convINFIXtoPostfix("(A + B + C) * D "));
+        assertEquals("A B C * + D * E - ", convINFIXtoPostfix("(A + B * C) * D - E "));
+        assertEquals("A B C * + D E / + ", convINFIXtoPostfix("(A + B * C) + D / E "));
 
-        String inStr24 = "A * B ^ C";
-        String outStr24 = convertInfixToPostfix(inStr24);
-        assertEquals("A B C ^ * ", outStr24);
+        assertEquals("A B * C D * + ", convINFIXtoPostfix("A * B + C * D"));
+        assertEquals("A B C / - D + ", convINFIXtoPostfix("A - B / C + D"));
 
-        String inStr25 = "A ^ B * C";
-        String outStr25 = convertInfixToPostfix(inStr25);
-        assertEquals("A B ^ C * ", outStr25);
+        assertEquals("A B + C + D + ", convINFIXtoPostfix("A + B + C + D"));
+        assertEquals("A B + C D + * ", convINFIXtoPostfix("(A + B) * (C + D)"));
 
-        String inStr26 = "(A + B + C) * D ";
-        String outStr26 = convertInfixToPostfix(inStr26);
-        assertEquals("A B + C + D * ", outStr26);
+        assertEquals("A B + K + C D + * ", convINFIXtoPostfix("(A + B + K) * (C + D)"));
+        assertEquals("A B ^ K + C D + * ", convINFIXtoPostfix("(A ^ B + K) * (C + D)"));
+        assertEquals("A B K * - C D + / ", convINFIXtoPostfix("(A - B * K) / (C + D)"));
+        assertEquals("A B * C + D E * + ", convINFIXtoPostfix("(A * B + C) + D * E"));
+        assertEquals("A B C * + D E * + ", convINFIXtoPostfix("(A + B * C) + D * E"));
 
-        String inStr27 = "(A + B * C) * D + E ";
-        String outStr27 = convertInfixToPostfix(inStr27);
-        assertEquals("A B C * + D * E + ", outStr27);
-
-        String inStr28 = "(A + B * C) + D * E ";
-        String outStr28 = convertInfixToPostfix(inStr28);
-        assertEquals("A B C * + D E * + ", outStr28);
-
-        String inStr29 = "(A + B + C) + D * (E + F) ";
-        String outStr29 = convertInfixToPostfix(inStr29);
+        String outStr29 = convINFIXtoPostfix("(A + B + C) + D * (E + F) ");
         assertEquals("A B + C + D E F + * + ", outStr29);
 
-        String inStr30 = "A + B * C + D * (E * F ^ G + H + I * J ^ K * L * (M ^ N * (P ^ Q + R)))";
-        String outStr30 = convertInfixToPostfix(inStr30);
-        assertEquals("A B C * + D E F G ^ * H + I J K ^ * L * M N ^ P Q ^ R + * * + * + ", outStr30);
+        String inStr11 = "(A + B * C - D) * (E + K * F / ( H + I ) )";
+        String outStr11 = convINFIXtoPostfix(inStr11);
+        assertEquals("A B C * + D - E K F * H I + / + * ", outStr11);
 
-
-        String outStr1 = convertInfixToPostfix(inStr1);
-        String outStr2 = convertInfixToPostfix(inStr2);
-        String outStr3 = convertInfixToPostfix(inStr3);
-        String outStr4 = convertInfixToPostfix(inStr4);
-        String outStr5 = convertInfixToPostfix(inStr5);
-        String outStr6 = convertInfixToPostfix(inStr6);
-        String outStr7 = convertInfixToPostfix(inStr7);
-        String outStr8 = convertInfixToPostfix(inStr8);
-        String outStr9 = convertInfixToPostfix(inStr9);
-        String outStr10 = convertInfixToPostfix(inStr10);
-        String outStr11 = convertInfixToPostfix(inStr11);
-        String outStr12 = convertInfixToPostfix(inStr12);
-
-        assertEquals("A B * C D * + ", outStr1);
-        assertEquals("A B C * + D + ", outStr2);
-        assertEquals("A B + C + D + ", outStr3);
-        assertEquals("A B + C D + * ", outStr4);
-        assertEquals("A B + K + C D + * ", outStr5);
-        assertEquals("A B ^ K + C D + * ", outStr6);
-        assertEquals("A B K * + C D + * ", outStr7);
-        assertEquals("A B * C + D E * + ", outStr8);
-        assertEquals("A B C * + D E * + ", outStr9);
-        assertEquals("A B C ^ * ", outStr10);
-        assertEquals("A B C * + D + E K F * H I + * + * ", outStr11);
+        String inStr12 = "A + B * C + D * (E + F ^ G * H + I * J ^ K * L * (M ^ N * (P ^ Q + R)))";
+        String outStr12 = convINFIXtoPostfix(inStr12);
         assertEquals("A B C * + D E F G ^ H * + I J K ^ * L * M N ^ P Q ^ R + * * + * + ", outStr12);
 
+        String inStr30 = "A + B / C + D / (E * F ^ G + H - I * J ^ K * L * (M ^ N * (P ^ Q + R)))";
+        String outStr30 = convINFIXtoPostfix(inStr30);
+        assertEquals("A B C / + D E F G ^ * H + I J K ^ * L * M N ^ P Q ^ R + * * - / + ", outStr30);
 
+
+    //Тест на перехват исключения при вводе недопустимой операции
+        try {
+            convINFIXtoPostfix("A < B");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+        try {
+            convINFIXtoPostfix("A . B");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+        try {
+            convINFIXtoPostfix("A + B . C");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+        try {
+            convINFIXtoPostfix("A * (B + C . D)");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+
+
+    //Тест на перехват исключения при пропуске операнда
+        try {
+            convINFIXtoPostfix("A * - B");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+        try {
+            convINFIXtoPostfix("A * B + / C");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+        try {
+            convINFIXtoPostfix("A * B + ");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+        try {
+            convINFIXtoPostfix(" * B + C");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+        try {
+            convINFIXtoPostfix("  ");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+
+
+    // Тест на перехват исключения при пропуске оператора
+        try {
+            convINFIXtoPostfix("A  B");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+        try {
+            convINFIXtoPostfix("A * B  C");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+
+
+    // Тест на перехват исключения при пропуске оператора после открывающей скобки
+        try {
+            convINFIXtoPostfix("A * B ( C");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+
+
+    // Тест на перехват исключения при пропуске операнда после открытия скобки
+        try {
+            convINFIXtoPostfix("A * B ( + C");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+
+        try {
+            convINFIXtoPostfix("A * B (B + ( * C)");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+
+        try {
+            convINFIXtoPostfix("()");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+
+
+    // Тест на перехват исключения при незакрытой скобке
+        try {
+            convINFIXtoPostfix("(");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+
+        try {
+            convINFIXtoPostfix(")");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+
+        try {
+            convINFIXtoPostfix("A * B * (C + D");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+
+        try {
+            convINFIXtoPostfix("A * B * (C + D * (E + F)");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+
+
+    // Тест на перехват исключения при вводе пустой строки
+        try {
+            convINFIXtoPostfix("");
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+
+
+    // Тест на перехват NullPointerException
+        try{
+            convINFIXtoPostfix(null);
+            fail("Should throw NullPointerException");
+        }
+        catch (NullPointerException e1) { }
     }
 
+    @Test
+    public void opOutTest() throws Exception {
+        StringBuilder output = new StringBuilder();
+
+        output.setLength(0);
+        opOut(output, 'A');
+        assertEquals( "A ", output.toString() );
+
+        opOut(output, 'B');
+        assertEquals( "A B ", output.toString() );
+
+        opOut(output, ' ');
+        assertEquals( "A B   ", output.toString() );
+
+        opOut(output, '*');
+        assertEquals( "A B   * ", output.toString() );
+    }
+
+    @Test
+    public void OperatorTest() throws Exception {
+    // Тест на успешное создание оператора
+        try { Operator oper1 = new Operator('+'); }
+        catch (IllegalArgumentException e1) { fail("Should create Operator"); }
+
+        try { Operator oper1 = new Operator('('); }
+        catch (IllegalArgumentException e1) { fail("Should create Operator"); }
+
+        try { Operator oper1 = new Operator('^'); }
+        catch (IllegalArgumentException e1) { fail("Should create Operator"); }
+
+    // Тест на перехват исключения при вводе недопустимого оператора
+        try {
+            Operator oper1 = new Operator(';');
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+
+        try {
+            Operator oper1 = new Operator('&');
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+
+        try {
+            Operator oper1 = new Operator('>');
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+
+        try {
+            Operator oper1 = new Operator(' ');
+            fail("Should throw IllegalArgumentException");
+        }
+        catch (IllegalArgumentException e1) { }
+    }
+
+    @Test
+    public void opSymbTest() throws Exception {
+        Operator oper1 = new Operator('+');
+        assertEquals('+', oper1.opSymb());
+
+        Operator oper2 = new Operator('^');
+        assertEquals('^', oper2.opSymb());
+    }
+
+    @Test
+    public void precTest() throws Exception {
+        Operator oper1 = new Operator('+');
+        assertEquals(1, oper1.prec());
+
+        Operator oper2 = new Operator('^');
+        assertEquals(3, oper2.prec());
+    }
 }
