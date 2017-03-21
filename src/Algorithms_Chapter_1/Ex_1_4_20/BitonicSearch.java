9289643d082acdaa79a8411de166922f8493bc06
v-log <v-log1313@mail.ru>
2017-03-22 00:44:44 +0700

    Бланк для решения 1.4.20

---
+++ src/Algorithms_Chapter_1/Ex_1_4_20/BitonicSearch.java
@@ -0,0 +1,15 @@
+package Algorithms_Chapter_1.Ex_1_4_20;
+
+/**
+ * Created by vl on 20.03.17.
+ */
+public class BitonicSearch {
+    public static boolean bitonicSearch(int[] a) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
