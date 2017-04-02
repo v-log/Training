75d066c5a778286bfbdb3ef2bc34941725634122
v-log <v-log1313@mail.ru>
2017-04-03 04:51:32 +0700

    Убрано свойство bitonicSearch() -  throws IllegalArgumentException

--- src/Algorithms_Chapter_1/Ex_1_4_20/BitonicSearch.java
+++ src/Algorithms_Chapter_1/Ex_1_4_20/BitonicSearch.java
@@ -4,8 +4,7 @@ package Algorithms_Chapter_1.Ex_1_4_20;
  * Created by vl on 20.03.17.
  */
 public class BitonicSearch {
-    public static boolean bitonicSearch(int[] bitonicArray, int key)
-            throws IllegalArgumentException {
+    public static boolean bitonicSearch(int[] bitonicArray, int key) {
 
         int maxIndex = bitonicMax(bitonicArray);
 
