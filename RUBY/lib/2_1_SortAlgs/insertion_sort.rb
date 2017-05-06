b8a4c27225053174125eb229c24e5a93a5c6ee39
v-log <v-log1313@mail.ru>
2017-05-06 23:44:58 +0700

    В insertion_sort в #insertion_sort и #insertion_sort2 исправлено условие выхода из внутреннего цикла; возвращаемое значение изменено с Enumerable на тип аргумента метода. В insertion_sort_test Expected аргументы заморожены.

--- RUBY/lib/2_1_SortAlgs/insertion_sort.rb
+++ RUBY/lib/2_1_SortAlgs/insertion_sort.rb
@@ -5,24 +5,26 @@ end
 def insertion_sort(a)
   n = a.size
   (1...n).each do |i|
-    (i).downto(1).each do |j|
-      if a[j] <= a[j - 1]
-        a[j], a[j - 1] = a[j - 1], a[j]
-      end
+    j = i
+    while j > 0 && a[j] <= a[j - 1]
+      a[j], a[j - 1] = a[j - 1], a[j]
+      j -= 1
     end
   end
+  a
 end
 
 def insertion_sort2(a)
   n = a.size
   get_var = block_given? ? lambda { |item| yield(item) } : lambda { |item| item }
   (1...n).each do |i|
-    (i).downto(1) do |j|
-      if get_var[a[j]] <= get_var[a[j - 1]]
-        a[j], a[j - 1] = a[j - 1], a[j]
-      end
+    j = i
+    while j > 0 && get_var[a[j]] <= get_var[a[j - 1]]
+      a[j], a[j - 1] = a[j - 1], a[j]
+      j -= 1
     end
   end
+  a
 end
 
 a = [5,3,2,1,4]
--- RUBY/tests/insertion_sort_test.rb
+++ RUBY/tests/insertion_sort_test.rb
@@ -4,8 +4,9 @@ require_relative '../lib/2_1_SortAlgs/insertion_sort'
 class InsertionSortTest < Minitest::Test
   def setup
     @unsorted_arr = [5,3,2,1,4]
-    @sorted_arr = [1,2,3,4,5]
+    @sorted_arr = [1,2,3,4,5].freeze
     @unsorted_hash = [['Vasya', 1], ['Petya', 3], ['Kolya', 2]]
+    @sorted_hash = [['Petya', 3], ['Kolya', 2], ['Vasya', 1]].freeze
   end
 
   # Testing #sorted?
@@ -19,17 +20,16 @@ class InsertionSortTest < Minitest::Test
 
   # Testing #insertion_sort
   def test_insertion_sort
-    assert_equal (1...5), insertion_sort(@unsorted_arr)
+    assert_equal @sorted_arr, insertion_sort(@unsorted_arr)
   end
 
   # Testing #insertion_sort2
   def test_insertion_sort2_without_block
-    assert_equal (1...5), insertion_sort2(@unsorted_arr)
+    assert_equal @sorted_arr, insertion_sort2(@unsorted_arr)
   end
 
   def test_insertion_sort2_with_block
     insertion_sort2(@unsorted_hash) { |item| -item[1] }
-    sorted_hash = [['Petya', 3], ['Kolya', 2], ['Vasya', 1]]
-    assert_equal sorted_hash, @unsorted_hash
+    assert_equal @sorted_hash, @unsorted_hash
   end
 end
