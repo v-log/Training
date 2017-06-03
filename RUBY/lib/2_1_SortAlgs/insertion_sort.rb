b7f48100a7828a7193b39797d18552eb3ebf7255
v-log <v-log1313@mail.ru>
2017-06-03 17:55:44 +0700

    В insertion_sort.rb к именам методов сортировок добавлены восклицательные знаки, так как они изменяют переданный объект. В insertion_sort_test.rb упрощен код тестов со случайными массивами.

--- RUBY/lib/2_1_SortAlgs/insertion_sort.rb
+++ RUBY/lib/2_1_SortAlgs/insertion_sort.rb
@@ -2,7 +2,7 @@ def sorted?(a)
   (0...a.size - 1).all? { |i| a[i] <= a[i + 1] }
 end
 
-def insertion_sort(a)
+def insertion_sort!(a)
   n = a.size
   (1...n).each do |i|
     j = i
@@ -14,7 +14,7 @@ def insertion_sort(a)
   a
 end
 
-def insertion_sort2(a)
+def insertion_sort2!(a)
   n = a.size
   get_var = block_given? ? lambda { |item| yield(item) } : lambda { |item| item }
   (1...n).each do |i|
@@ -29,18 +29,10 @@ end
 
 a = [5,3,2,1,4]
 puts sorted?(a)
-insertion_sort(a)
+insertion_sort!(a)
 raise unless sorted?(a)
 puts a.inspect
 
 b = [['Vasya', 1], ['Petya', 3], ['Kolya', 2]]
-insertion_sort2(b) { |item| -item[1] }
+insertion_sort2!(b) { |item| -item[1] }
 puts b.inspect # [["Petya", 3], ["Kolya", 2], ["Vasya", 1]]
-
-rand_hash_1 = {}
-100.times { |n| rand_hash_1[n] = rand(0..99) }
-rand_hash_2 = rand_hash_1.clone
-insertion_sort2(rand_hash_1) { |item| -item[1] }
-# puts rand_hash_1.inspect
-puts
-# puts rand_hash_2.inspect
--- RUBY/tests/insertion_sort_test.rb
+++ RUBY/tests/insertion_sort_test.rb
@@ -3,55 +3,55 @@ require_relative '../lib/2_1_SortAlgs/insertion_sort'
 
 class InsertionSortTest < Minitest::Test
   def setup
-    @unsorted_arr = [5,3,2,1,4]
-    @sorted_arr = [1,2,3,4,5].freeze
+    @unsorted_ary = [5,3,2,1,4]
+    @sorted_ary = [1,2,3,4,5].freeze
     @unsorted_hash = [['Vasya', 1], ['Petya', 3], ['Kolya', 2]]
     @sorted_hash = [['Petya', 3], ['Kolya', 2], ['Vasya', 1]].freeze
   end
 
   # Testing #sorted?
   def test_sorted
-    assert sorted?(@sorted_arr), "Should be sorted"
+    assert sorted?(@sorted_ary), "Should be sorted"
   end
 
   def test_not_sorted
-    refute sorted?(@unsorted_arr), "Shouldn't be sorted"
+    refute sorted?(@unsorted_ary), "Shouldn't be sorted"
   end
 
-  # Testing #insertion_sort
+  # Testing #insertion_sort!
   def test_insertion_sort
-    assert_equal @sorted_arr, insertion_sort(@unsorted_arr)
+    assert_equal @sorted_ary, insertion_sort!(@unsorted_ary)
   end
 
-  def test_insertion_sort_with_random_array
-    rand_arr_1 = Array.new(100) { rand(0...99) }
-    rand_arr_2 = rand_arr_1[0..rand_arr_1.size]
-    assert_equal rand_arr_1.sort, insertion_sort(rand_arr_2)
+  def test_insertion_sort_with_random_aryay
+    rand_ary_1 = Array.new(100) { rand(0...99) }
+    rand_ary_2 = rand_ary_1[0..rand_ary_1.size]
+    assert_equal rand_ary_1.sort, insertion_sort!(rand_ary_2)
   end
 
-  # Testing #insertion_sort2
+  # Testing #insertion_sort2!!
   def test_insertion_sort2_without_block
-    assert_equal @sorted_arr, insertion_sort2(@unsorted_arr)
+    assert_equal @sorted_ary, insertion_sort2!(@unsorted_ary)
   end
 
-  def test_insertion_sort2_without_block_with_rand_arr
-    rand_arr_1 = Array.new(100) { rand(0..99) }
-    rand_arr_2 = rand_arr_1[0..rand_arr_1.size]
-    assert_equal rand_arr_1.sort, insertion_sort2(rand_arr_2)
+  def test_insertion_sort2_without_block_with_rand_ary
+    rand_ary_1 = (0...100).to_a.shuffle
+    rand_ary_2 = rand_ary_1.dup
+    assert_equal rand_ary_1.sort, insertion_sort2!(rand_ary_2)
   end
 
   def test_insertion_sort2_with_block
-    insertion_sort2(@unsorted_hash) { |item| -item[1] }
+    insertion_sort2!(@unsorted_hash) { |item| -item[1] }
     assert_equal @sorted_hash, @unsorted_hash
   end
 
-  def test_insertion_sort2_with_block_with_rand_arr
-    rand_hash_1 = {}
-    shuffled_indices = (0..99).to_a.shuffle
-    100.times { |n| rand_hash_1[n] = shuffled_indices.pop }
-    rand_hash_2 = rand_hash_1.clone.to_a
-
-    insertion_sort2(rand_hash_2) { |item| -item[1] }
-    assert_equal(rand_hash_1.sort_by { |key, value| -value }, rand_hash_2)
+  def test_insertion_sort2_with_block_with_rand_ary
+    permutation = (0...100).to_a.shuffle
+    pairs = []
+    (0...permutation.size).each { |i| pairs.push [i, permutation[i]] }
+    pairs_copy = pairs.dup
+    pairs.sort_by! { |item| -item[1] }
+    insertion_sort2!(pairs_copy) { |item| -item[1] }
+    assert_equal(pairs, pairs_copy)
   end
 end
