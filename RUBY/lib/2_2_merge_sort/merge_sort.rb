9b9db816189fa680e63209cbd8f59deab4fdee2b
v-log <v-log1313@mail.ru>
2018-02-05 02:27:55 +0300

    Remove unnecessary blank lines. Change merge! method name.

--- RUBY/lib/2_2_merge_sort/merge_sort.rb
+++ RUBY/lib/2_2_merge_sort/merge_sort.rb
@@ -10,7 +10,6 @@ def merge_sort!(a)
   else
     merge_sort_hlpr!(a, 0, a.size - 1, aux)
   end
-
   a
 end
 
@@ -25,17 +24,16 @@ def merge_sort_hlpr!(a, lo, hi, aux)
   if block_given?
     merge_sort_hlpr!(a, lo, mid, aux, &Proc.new)
     merge_sort_hlpr!(a, mid + 1, hi, aux, &Proc.new)
-    merge_2!(a, lo, mid, hi, aux, &Proc.new)
+    merge!(a, lo, mid, hi, aux, &Proc.new)
   else
     merge_sort_hlpr!(a, lo, mid, aux)
     merge_sort_hlpr!(a, mid + 1, hi, aux)
-    merge_2!(a, lo, mid, hi, aux)
+    merge!(a, lo, mid, hi, aux)
   end
-
   a
 end
 
-def merge_2! (a, lo, mid, hi, aux)
+def merge! (a, lo, mid, hi, aux)
   i = lo
   j = mid + 1
   (lo..hi).each do |x|
@@ -43,7 +41,6 @@ def merge_2! (a, lo, mid, hi, aux)
   end
 
   get_var = block_given? ? lambda { |item| yield (item) } : lambda { |item| item }
-
   (lo..hi).each do |x|
     if i > mid
       a[x] = aux[j]
@@ -59,7 +56,6 @@ def merge_2! (a, lo, mid, hi, aux)
       i += 1
     end
   end
-
   a
 end
 
@@ -70,7 +66,8 @@ merge_sort!(a)
 raise unless sorted?(a)
 puts "result:" + "\n" + a.inspect
 puts sorted?(a).to_s
-puts b = [['Vasya', 1], ['Petya', 3], ['Kolya', 2]]
+puts 
+b = [['Vasya', 1], ['Petya', 3], ['Kolya', 2]]
 puts b.inspect
 merge_sort!(b) { |item| -item[1] }
 puts "result:" + "\n" + b.inspect # [["Petya", 3], ["Kolya", 2], ["Vasya", 1]]
