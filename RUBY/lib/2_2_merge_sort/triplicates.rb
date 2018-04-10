6123bf1fa86b0074849f42c113eaa4a4ccb169cc
v-log <v-log1313@mail.ru>
2018-04-10 11:04:01 +0300

    Triplicates search implementation

--- RUBY/lib/2_2_merge_sort/triplicates.rb
+++ RUBY/lib/2_2_merge_sort/triplicates.rb
@@ -1,62 +1,74 @@
+require_relative 'merge_sort'
+
 def sorted?(a)
   (0...a.size - 1).all? { |i| a[i] <= a[i + 1] }
 end
 
-def less(a, b)
-  return a if a < b
-  b
-end
-
 def triplicates(*lists)
-  lists_size = 0
-  lists.each do |x|
-    lists_size += x.size
+  return nil if lists.size <= 1
+
+  # sort input lists
+  lists.each do |list|
+    merge_sort!(list)
+    #list.sort
   end
   
-  indexes = Array.new(lists.size, 0)
-  k = 0
-  cur = ""
-  lists_size.times do |x|
-    temp = Array.new
-    lists.each_with_index do |item, index|
-      if indexes[index] < item.size
-        temp << item[indexes[index]]
+  # find_triplicates
+  find_triplicates(lists)
+end 
+
+private
+
+def total_size(lists)
+  total_size = 0
+  lists.each do |list|
+    total_size += list.size
+  end
+  total_size
+end
+
+def find_triplicates(given_lists)
+  lists_total_size = total_size(given_lists)
+  indices = Array.new(given_lists.size, 0)
+  eql_elems = 0
+  prev = ""
+  
+  lists_total_size.times do |curr|
+    # look through all lists finding min element
+    curr_min_elems = Array.new
+    given_lists.each_with_index do |list, list_number|
+      if indices[list_number] < list.size
+        curr_min_elems << list[indices[list_number]]
       end
     end
     
-    x = min(temp)
-    lists.each_with_index do |z, index|
-      if z[indexes[index]].eql?(x)
-        indexes[index] += 1
+    curr = curr_min_elems.min
+    
+    # inc index in a list with min elem
+    given_lists.each_with_index do |list, list_number|
+      list_elem = list[indices[list_number]]
+      if list_elem.eql?(curr)
+        indices[list_number] += 1
         break
       end
     end
     
-    if x.eql?(cur)
-      k += 1
+    if curr.eql?(prev)
+      eql_elems += 1
     else
-      k = 0
+      eql_elems = 0
     end
+
+    prev = curr
     
-    cur = x
-    return x if k == 2
+    return curr if eql_elems == 2
   end
 
   nil
 end
 
-private
-
-def min(p)
-  min = p[0]
-  p.each do |x|
-    min = x if x < min
-  end
-  
-  min
-end
-
 a = ["abcd", "bcde", "cdef"]
 b = ["cdef", "defg", "efgh"]
 c = ["aghi", "bhij", "cdef"]
+#d = triplicates(a, b, c)
 puts "triplicate = #{triplicates(a, b, c)}"
