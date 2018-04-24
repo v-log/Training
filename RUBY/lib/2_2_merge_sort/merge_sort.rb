de9891725c93d86e4cac58dee1edd359c3b270b5
v-log <v-log1313@mail.ru>
2018-04-24 19:05:17 +0300

    Temporary save. Working on improvement 3

--- RUBY/lib/2_2_merge_sort/merge_sort.rb
+++ RUBY/lib/2_2_merge_sort/merge_sort.rb
@@ -27,20 +27,51 @@ def choose_sort(a, lo,  hi, aux, &block)
   end
 end
 
+#improvement 3
+def swap(a, b)
+  a, b = b, a
+end
+
 def merge_sort_hlpr!(a, lo, hi, aux, &block)
   if hi <= lo
     return a
   end
 
   mid = lo + (hi - lo)/2
-  #merge_sort_hlpr!(a, lo, mid, aux, &block)
-  #merge_sort_hlpr!(a, mid + 1, hi, aux, &block)
-  choose_sort(a, lo, mid, aux, &block)
-  choose_sort(a, mid + 1, hi, aux, &block)
+  
+  puts a.inspect
+  puts aux.inspect
+  puts
+
+  merge_sort_hlpr!(a, lo, mid, aux, &block)
+  swap(a, aux)
+
+  puts "after swap 1"
+  puts a.inspect
+  puts aux.inspect
+  puts
+
+  merge_sort_hlpr!(a, mid + 1, hi, aux, &block)
+  swap(a, aux)
+
+  puts "after swap 2"
+  puts a.inspect
+  puts aux.inspect
+  puts
+
+  #choose_sort(a, lo, mid, aux, &block)
+  #choose_sort(a, mid + 1, hi, aux, &block)
 
   #improvement 2
   if block.call(a[mid]) > block.call(a[mid + 1])
     merge!(a, lo, mid, hi, aux, &block)
+    swap(a, aux)
+
+    puts "after swap 3"
+    puts a.inspect
+    puts aux.inspect
+    puts
+
   end
 
   a
@@ -49,29 +80,29 @@ end
 def merge! (a, lo, mid, hi, aux, &block)
   i = lo
   j = mid + 1
-  (lo..hi).each do |x|
-    aux[x] = a[x]
-  end
+#  (lo..hi).each do |x|
+#    aux[x] = a[x]
+#  end
 
   (lo..hi).each do |x|
     if i > mid
-      a[x] = aux[j]
+      aux[x] = a[j]
       j += 1
     elsif j > hi
-      a[x] = aux[i]
+      aux[x] = a[i]
       i += 1
-    elsif block.call(aux[j]) < block.call(aux[i])
-      a[x] = aux[j]
+    elsif block.call(a[j]) < block.call(a[i])
+      aux[x] = a[j]
       j += 1
     else
-      a[x] = aux[i]
+      aux[x] = a[i]
       i += 1
     end
   end
   a
 end
 
-a = (0..30).to_a.shuffle
+a = (0...8).to_a.shuffle
 puts a.inspect
 puts sorted?(a)
 merge_sort!(a)
