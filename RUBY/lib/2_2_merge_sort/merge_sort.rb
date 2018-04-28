52dbd5e16e50b7ca81ac0b10cb2311a18164191b
v-log <v-log1313@mail.ru>
2018-04-28 19:24:31 +0300

    Modify #insertion_sort to copy sorted array to auxiliary array if passed. Complete work on improvement 3 draft.

--- RUBY/lib/2_2_merge_sort/merge_sort.rb
+++ RUBY/lib/2_2_merge_sort/merge_sort.rb
@@ -34,8 +34,11 @@ private
 def levels(array)
   # Count source array depth to set the 
   # right initial position
+
+  #We have the first level whatever the case
   levels = 1
   pow_of_2 = 1
+
   while array.size > pow_of_2 do
     pow_of_2 *= 2
     levels += 1
@@ -44,21 +47,53 @@ def levels(array)
 end
 
 # Improvement 1
+=begin
 def choose_sort(a, lo, hi, aux, level, cutoff, &block)
-  if hi - lo <= cutoff
+  if hi - lo - 1 <= cutoff
+    puts "for ins_sort lo = #{lo}, hi = #{hi}"
     puts "Before ins_sort a = #{a.inspect}"
     insertion_sort_2!(a, lo, hi, &block)
+    level += 1
     puts "After ins_sort a  = #{a.inspect}"
   else
     merge_sort_hlpr!(a, lo, hi, aux, level, cutoff, &block)
   end
 end
+=end
 
-def merge_sort_hlpr!(a, lo, hi, aux,level, cutoff, &block)
+def merge_sort_hlpr!(a, lo, hi, aux, level, cutoff, &block)
   level -= 1
 
+  # Choose insertion sort if optimal
+
+  # Add 1 to count the zero index
+  if hi - lo + 1 <= cutoff
+    puts "For ins_sort lo = #{lo}, hi = #{hi}"
+    puts "Before ins_sort! a = #{a.inspect}"
+    puts "aux        =         #{aux.inspect}"
+    puts "Level = #{level}"
+    puts "Level even = #{level.even?}"
+    if level.even?
+      insertion_sort_2!(aux, lo, hi, a, &block)
+    else
+      insertion_sort_2!(a, lo, hi, aux, &block)
+    end
+    
+    puts "After ins_sort! a  = #{a.inspect}"
+    puts "aux        =         #{aux.inspect}"
+    level += 1
+    return
+  end
+
   puts "level = #{level}"
+  # Return when reach bottom level (1 element)
   if hi <= lo
+    # If array size is odd, one element, which is
+    # supposed to appear at bottom level, will
+    # do one level higher, which means it must be
+    # merged with something, but there is nil.
+    # We omit this merge by copying only existing
+    # element.
     aux[lo] = a[lo] if level != 0
     level += 1
     return
@@ -72,16 +107,15 @@ def merge_sort_hlpr!(a, lo, hi, aux,level, cutoff, &block)
 
   # Improvement 3 - change between arrays
   # in order to avoid unnecessary data copy
-  #merge_sort_hlpr!(aux, lo, mid, a, level, &block)
-
+  merge_sort_hlpr!(aux, lo, mid, a, level, cutoff, &block)
+  #choose_sort(aux, lo, mid, a, level, cutoff, &block)
   puts "After sorting left half"
   puts "a   = #{a.inspect}"
   puts "aux = #{aux.inspect}"
   puts
 
-  #merge_sort_hlpr!(aux, mid + 1, hi, a, level, &block)
-  choose_sort(aux, lo, mid, a, level, cutoff, &block)
-  choose_sort(aux, mid + 1, hi, a, level, cutoff, &block)
+  merge_sort_hlpr!(aux, mid + 1, hi, a, level, cutoff, &block)
+  #choose_sort(aux, mid + 1, hi, a, level, cutoff, &block)
 
   puts "After sorting right half"
   puts "a   = #{a.inspect}"
@@ -141,7 +175,7 @@ def merge!(a, lo, mid, hi, aux, &block)
   #a, aux = aux, a
 end
 
-a = (0...3).to_a.shuffle
+a = (0...9).to_a.shuffle
 puts a.inspect
 puts sorted?(a)
 merge_sort!(a)
