fdcd1d54fb369252b2fa7fc63785da3cb10d5392
v-log <v-log1313@mail.ru>
2018-05-15 14:07:40 +0300

    Merge-sort improvements implementation

--- RUBY/lib/2_2_merge_sort/merge_sort.rb
+++ RUBY/lib/2_2_merge_sort/merge_sort.rb
@@ -1,95 +1,186 @@
-module MergeSort
-  def sorted?(a)
-    (0...a.size - 1).all? { |i| a[i] <= a[i + 1] }
+require_relative "../2_1_sort_algs/insertion_sort"
+
+def sorted?(a)
+  (0...a.size - 1).all? { |i| a[i] <= a[i + 1] }
+end
+
+def merge_sort!(a, cutoff = 16, &block)
+  return a if a.size <= 1
+
+  # Auxiliary array for merge sort
+  aux = Array.new(a.size)
+
+  # Set the right initial position of arrays
+  # in order to merge from source to axiliary
+  # array at the bottom level merge
+  level = levels(a)
+  a, aux = aux, a if level%2 != 0
+
+  if block_given?
+    merge_sort_hlpr!(a, 0, a.size - 1, aux, level, cutoff, &block)
+  else
+    merge_sort_hlpr!(a, 0, a.size - 1, aux, level, cutoff, &Proc.new {|x| x})
   end
 
-  def merge_sort!(a, &block)
-    return a if a.size <= 1
-    aux = Array.new(a.size)
-
-    if block_given?
-      merge_sort_hlpr!(a, 0, a.size - 1, aux, &block)
-    else
-      merge_sort_hlpr!(a, 0, a.size - 1, aux, &Proc.new { |x| x })
-    end
-    a
+  # Duplicate arrays in case we get to return
+  # the unsorted one after the last merge
+  (0...a.length).each do |x|
+    a[x] = aux[x]
   end
+  a.to_a
+end
 
-  def merge_sort_bu!(a, &block)
-    return a if a.size <= 1
-    n = a.length
-    aux = Array.new(n)
-    sz = 1
+def merge_sort_bu!(a, &block)
+  return a if a.size <= 1
+  n = a.length
+  aux = Array.new(n)
+  sz = 1
 
-    while sz < n do
-      0.step(n - sz, 2 * sz) do |lo|
-        hi = less(lo + sz + sz - 1, n - 1)
+  while sz < n do
+    0.step(n - sz, 2 * sz) do |lo|
+      hi = less(lo + sz + sz - 1, n - 1)
+
+      # If array size is odd, there is one last 
+      # element which has no pair, i.e. it has
+      # nothing to merge with. We thus need to 
+      # manually copy this single element
+      if a[a.size - 1] == nil && a[a.size - 2] != nil
+        a[lo] = aux[lo]
+      elsif aux[aux.size - 1] == nil && aux[aux.size - 2] != nil
+        aux[lo] = a[lo]
+      else
+        # Normal merge for non-nil elements
+        mid = lo + sz - 1
         if block_given?
-          merge!(a, lo, lo + sz - 1, hi, aux, &block)
+          merge!(a, lo, mid, hi, aux, &block)
         else
-          merge!(a, lo, lo + sz - 1, hi, aux) { |x| x }
+          merge!(a, lo, mid, hi, aux) { |x| x }
         end
-      end      
-      sz += sz
+      end
     end
-    a
+    sz += sz
+    
+    # Improvement 3 - change between arrays
+    # in order to avoid unnecessary data copy
+    a, aux = aux, a
   end
 
-  private
-
-  def less(a, b)
-    a < b ? a : b
+  # Duplicate arrays in case we get to return
+  # the unsorted one after the last merge 
+  (0...a.length).each do |x|
+    aux[x] = a[x]
   end
+  a.to_a
+end
 
-  def merge_sort_hlpr!(a, lo, hi, aux, &block)
-    if hi <= lo
-      return a
+private
+
+def less(a, b)
+  a < b ? a : b
+end
+
+def levels(array)
+  # Count source array depth to set the 
+  # right initial position
+
+  # The first level is there whatever the case
+  levels = 1
+  pow_of_2 = 1
+
+  while array.size > pow_of_2 do
+    pow_of_2 *= 2
+    levels += 1
+  end
+  levels
+end
+
+def merge_sort_hlpr!(a, lo, hi, aux, level, cutoff, &block)
+  level -= 1
+
+  # Improvement 1
+  # Choose insertion sort if optimal
+
+  # Add 1 to count the zero index
+  if hi - lo + 1 <= cutoff
+    # Set the right position of source and
+    # destination arrays depending on level parity
+    # (need this due to improvement 3 - change
+    # between source and destination areays)
+    if level.even?
+      insertion_sort_2!(aux, lo, hi, a, &block)
+    else
+      insertion_sort_2!(a, lo, hi, aux, &block)
     end
 
-    mid = lo + (hi - lo)/2
-    merge_sort_hlpr!(a, lo, mid, aux, &block)
-    merge_sort_hlpr!(a, mid + 1, hi, aux, &block)
+    level += 1
+    return
+  end
+
+  # Return when reach bottom level (1 element)
+  if hi <= lo
+    # If array size is odd, one element, which is
+    # supposed to appear at bottom level, will
+    # do one level higher, which means it must be
+    # merged with something, but there is nil.
+    # We omit this merge by only copying existing
+    # element.
+    aux[lo] = a[lo] if level != 0
+    level += 1
+    return
+  end
+
+  mid = lo + (hi - lo)/2
+
+  # Improvement 3 - change between arrays
+  # in order to avoid unnecessary data copy
+  merge_sort_hlpr!(aux, lo, mid, a, level, cutoff, &block)
+  merge_sort_hlpr!(aux, mid + 1, hi, a, level, cutoff, &block)
+
+  # Improvement 2
+  # Check if subarrays are already in order
+  if block.call(a[mid]) > block.call(a[mid + 1])
     merge!(a, lo, mid, hi, aux, &block)
   end
 
-  def merge! (a, lo, mid, hi, aux, &block)
-    i = lo
-    j = mid + 1
-    (lo..hi).each do |x|
-      aux[x] = a[x]
-    end
+  level += 1
+end
 
-    (lo..hi).each do |x|
-      if i > mid
-        a[x] = aux[j]
-        j += 1
-      elsif j > hi
-        a[x] = aux[i]
-        i += 1
-      elsif block.call(aux[j]) < block.call(aux[i])
-        a[x] = aux[j]
-        j += 1
-      else
-        a[x] = aux[i]
-        i += 1
-      end
+def merge!(a, lo, mid, hi, aux, &block)
+  # Return if merging single element
+  if hi <= lo
+    return
+  end
+
+  i = lo
+  j = mid + 1
+
+  (lo..hi).each do |x|
+    if i > mid
+      aux[x] = a[j]
+      j += 1
+    elsif j > hi
+      aux[x] = a[i]
+      i += 1
+    elsif block.call(a[j]) < block.call(a[i])
+      aux[x] = a[j]
+      j += 1
+    else
+      aux[x] = a[i]
+      i += 1
     end
   end
 end
 
-include MergeSort
-
-a = (0..30).to_a.shuffle
+a = (0...7).to_a.shuffle
 puts a.inspect
 puts sorted?(a)
-#merge_sort!(a)
 merge_sort_bu!(a)
+puts a.inspect
 raise unless sorted?(a)
 puts "result:" + "\n" + a.inspect
-puts sorted?(a)
+puts sorted?(a).to_s
 puts 
 b = [['Vasya', 1], ['Petya', 3], ['Kolya', 2]]
 puts b.inspect
-#merge_sort!(b) { |item| -item[1] }
-merge_sort_bu!(b) { |item| -item[1] }
+merge_sort!(b) { |item| -item[1] }
 puts "result:" + "\n" + b.inspect # [["Petya", 3], ["Kolya", 2], ["Vasya", 1]]
--- RUBY/tests/2_2_sort_algs/sort_test.rb
+++ RUBY/tests/2_2_sort_algs/sort_test.rb
@@ -2,8 +2,9 @@ require 'minitest/autorun'
 
 module SortTestModule
   def setup
-    @unsorted_ary = [5,3,2,1,4,9,6,8,7]
-    @sorted_ary = [1,2,3,4,5,6,7,8,9].freeze
+    @sorted_ary = [1,2,3,4,5,6,7,8,9]
+    @unsorted_ary = [6,2,8,3,1,5,4,9,7]
+    @ary_sizes = [0,1,2,3,4,7,8,9,15,16,17,31,32,33,63,64,65].freeze
     @unsorted_hash = [['Vasya', 1], ['Petya', 3], ['Kolya', 2]]
     @sorted_hash = [['Petya', 3], ['Kolya', 2], ['Vasya', 1]].freeze
   end
@@ -19,22 +20,22 @@ module SortTestModule
 
   # Testing #sort!
   def sort!()
-    raise NotImplementedError, "To be implemented in child class"
-  end
-
-  def test_sort_like_without_block
-      assert_equal @sorted_ary, sort!(@unsorted_ary) { |x| x}, "Array hasn't been sorted"
+    raise NotImplementedError, "To be implemented in child test module"
   end
 
   def test_sort_like_without_block_with_rand_ary
-    rand_ary_1 = (0...100).to_a.shuffle
-    rand_ary_2 = rand_ary_1.dup
-    assert_equal rand_ary_1.sort, sort!(rand_ary_2) { |x| x }, "Array hasn't been sorted properly"
+    @ary_sizes.each do |ary_size|
+      rand_ary_1 = (0..ary_size).to_a.shuffle
+      rand_ary_2 = rand_ary_1.dup
+      assert_equal rand_ary_1.sort, sort!(rand_ary_2),
+      "Array hasn't been sorted properly\n #{rand_ary_2.inspect}"
+    end
   end
 
   def test_sort_with_block
     sort!(@unsorted_hash) { |item| -item[1] }
-    assert_equal @sorted_hash, @unsorted_hash, "Array hasn't been sorted"
+    assert_equal @sorted_hash, @unsorted_hash, 
+    "Hash hasn't been sorted properly\n #{@unsorted_hash.inspect}\n block = { |item| -item[1] }"
   end
 
   def test_sort_with_block_with_rand_ary
@@ -43,6 +44,7 @@ module SortTestModule
     pairs_copy = pairs.dup
     pairs.sort_by! { |item| -item[1] }
     sort!(pairs_copy) { |item| -item[1] }
-    assert_equal pairs, pairs_copy, "Array hasn't been sorted properly"
+    assert_equal pairs, pairs_copy,
+    "Array hasn't been sorted properly\n #{pairs_copy.inspect}\n block = { |item| -item[1] }"
   end
 end
