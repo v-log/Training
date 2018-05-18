6d1a2aae7a0d0c36e4cf55fd744d7bde6dfc4b78
v-log <v-log1313@mail.ru>
2018-05-18 19:10:18 +0300

    Change #triplicates to #common_elem, change algorithm to work with more than three lists. Improve explanation comments

--- RUBY/lib/2_2_merge_sort/triplicates.rb
+++ RUBY/lib/2_2_merge_sort/triplicates.rb
@@ -1,31 +1,44 @@
 require_relative 'merge_sort'
 
-# Defines if 'a' is sorted
-def sorted?(a)
-  (0...a.size - 1).all? { |i| a[i] <= a[i + 1] }
-end
-
-# General idea is to find three equal elements
-# (triplicate), one per list, by sorting each list
-# and looking through trinities of lists' minimal
-# elements one by one, until we find first trinity
-# with all elements being equal.
-def triplicates(*lists)
+# Public: Determine if there is any element 
+# common to all given lists of Comparable
+# elements, and if so, return first such element.
+#
+# lists - Zero or more lists to search for triplicate in
+#
+# Exapmles
+#
+#   a = ["ccc", "aaa", "bbb"]
+#   b = ["kkk", "bbb", "jjj"]
+#   c = ["bbb", "zzz", "yyy"]
+#   common_elem(a, b, c)
+#   # => "bbb"
+#
+# Returns the Comparable common element or
+#   nil if none was found
+def common_elem(*lists)
   return nil if lists.size <= 1
 
-  # sort input lists
+  # Sort input lists
   lists.each do |list|
     merge_sort!(list)
   end
   
-  # find_triplicates
-  find_triplicates(lists)
+  # Find first common element in sorted lists
+  find_common_elem(lists)
 end 
 
 private
 
+# Explicit method to define min element
+# in the array with nil elements
 def min_with_nils(list)
+  # Minimum's initial value
   min = ""
+
+  # Set the existing (in the array) initial
+  # value for minimum (in order to be able
+  # to use classic algorithm of finding minimum)
   list.each do |elem|
     if elem == nil
       next
@@ -35,34 +48,47 @@ def min_with_nils(list)
     end
   end  
 
+  # Find minimum element
   list.each do |elem|
     next if elem == nil
     min = elem if elem < min
   end
+  
+  # Return min if found
   return min if min != ""
+
+  # Return nil if no min found
+  # (i.e. all elements are nils)
   nil
 end
 
-# Having all lists sorted, we take first
-# elements of each list, find min element;
-# in the list with min element move on to
+# Having all lists sorted, take first
+# elements of each list, find min element
+# of those;
+#
+# In the list with min element move on to
 # the next element getting next trinity of
-# elements, where we find min element, and
-# so on, until we get 3 equal min elements.
-
-# To eliminate the case, when there are
-# several equal elements (which might be
-# defined as part of triplicates), there
-# is a skipping stage, checking if next
-# element after min is equal to min.
-def find_triplicates(given_lists)
+# elements, where find min element, and
+# so on, until 3 equal min elements found.
+#
+# To eliminate the case, when duplicates
+# in one list might be defined common elements,
+# there is a skipping stage, checking if next
+# element after current min is equal to min,
+# and if so, incrementing the list's index.
+def find_common_elem(given_lists)
   lists_total_size = given_lists.map(&:size).sum
-  # indices of all lists to move through them
+
+  # Indices of all lists to move through them
   indices = Array.new(given_lists.size, 0)
-  # equal elements counter - inc when coincidence found
+
+  # Equal elements counter - inc when coincidence found
   # (1  - for current element)
   eql_elems = 1
+
+  # Previous minimum initial value
   prev_min = ""
+
   
   lists_total_size.times do
     # Take first elements of sorted lists
@@ -77,46 +103,29 @@ def find_triplicates(given_lists)
       end
     end
 
-    puts "curr_min_elems = #{curr_min_elems}"
-
     # Current min element out of all lists
-#    curr_min = curr_min_elems.min
     curr_min = min_with_nils(curr_min_elems)
     return nil if curr_min == nil
-    puts "curr_min = #{curr_min}"
     
     # In a list with min element found inc the index
     # while elements after min are equal to min
-    # (to eliminate counting copies in one list as
-    # a part of triplicate)
-=begin
-    given_lists.each_with_index do |list, list_number|
-      list_elem = list[indices[list_number]]
-      
-      if list_elem == curr
-        indices[list_number] += 1
-        break
-      end
-    end
-=end
-
+    # (to eliminate counting duplicates in one list
+    # as common elements)
     curr_min_elems.each_with_index do |elem, list_number|
       next if elem == nil
-#      puts "continue"
+
+      # Define the list containing min element
       if elem == curr_min
+        
+        # List with min element (for readability)
         list = given_lists[list_number]
-        index = indices[list_number]
 
-        puts "list with min elem = #{list_number}"
-
-#        indices[list_number] += 1
-#        next_elem = list[index + 1]
-
+        # Skipping stage
         while list[indices[list_number]] == curr_min do
-#              indices[list_number] == list.size - 1 do
           indices[list_number] += 1
         end
 
+        # Break when min found
         break
       end
     end
@@ -128,11 +137,12 @@ def find_triplicates(given_lists)
       eql_elems = 1
     end
 
+    # Update previous element
     prev_min = curr_min
 
     # Exit when first three equal elements
     # in different arrays are found
-    return curr_min if eql_elems == 3
+    return curr_min if eql_elems == given_lists.size
   end
 
   # If no triplicates found
@@ -147,4 +157,4 @@ p a.inspect
 p b.inspect
 p c.inspect
 puts
-p "triplicate = #{triplicates(a, b, c)}"
+p "triplicate = #{common_elem(a, b, c)}"
